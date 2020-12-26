e6747ded0376da1fc15beb27e7f69ad155937e09
James Pinckney <james.r.pinckney@gmail.com>
2020-12-26 00:01:37 -0500

    Solve Part 2

---
+++ Day_09/part2.py
@@ -0,0 +1,42 @@
+import copy
+with open("c:\\Users\\madri\\Documents\\Advent of Code\\Day_09\\xmas.txt", "r") as f:
+    codes = f.read()
+codes = codes.split("\n")
+codes = [int(c) for c in codes]
+
+header = codes[0: 25:]
+codes = codes[25:]
+
+def is_divisible(code, preamble):
+    i = 0
+    while i < len(preamble):
+        j = 0
+        while j < len(preamble):
+            num = preamble[i]+preamble[j]
+            if num == code:
+                return True
+            j += 1
+        i += 1
+    return False
+
+def find_bad_number(preamble, code_list):
+    if not is_divisible(code_list[0], preamble=preamble):
+        return code_list[0]
+    else:
+        new_preamble = preamble[1:]
+        new_preamble.append(code_list[0])
+        return find_bad_number(preamble=new_preamble, code_list=code_list[1:])
+
+def find_bad_num_sum(codes, bad_num, i):
+    badsum = 0
+    j = i
+    while badsum < bad_num:
+        badsum += codes[j]
+        if badsum == bad_num:
+            return (codes[i],codes[j])
+        j += 1
+    return find_bad_num_sum(codes=codes,bad_num=bad_num, i=i+1)
+    
+
+
+print(find_bad_num_sum(codes=codes,bad_num=find_bad_number(preamble=header,code_list=codes),i=0))
